c0adb262a50b68827d9c58030bd2cb5ae3e198ee
Aniket Kayande <aniket.kayande@gmail.com>
2018-07-07 23:08:42 +0530

    [Update] implemented HTTPS [Inprogres] google sign-in

--- InfernoEngine/app.js
+++ InfernoEngine/app.js
@@ -1,15 +1,37 @@
 var express = require('express');
 var ejs = require('ejs');
+var fs = require('fs');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var https = require('https');
+var http = require('http');
+var database = require('./utils/database');
+var userModule = require('./models/user');
 
+var key = fs.readFileSync('encryption/server.key');
+var cert = fs.readFileSync( 'encryption/server.pem' );
+
+var httpsPort = 8443;
+var options = { 
+    key : key,
+    cert : cert,
+    passphrase: 'admin'
+};
 var app = express();
+var router = express.Router();
 app.use(express.static("styles"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-var database = require('./utils/database');
-var userModule = require('./models/user');
+app.use(function(req,res,next)
+{
+    if(req.secure)
+    {
+        return next();
+    };
+    
+    res.redirect('https://' + req.hostname +":"+ httpsPort + req.url);
+});
 
 app.get("/", function (req, res) {
     var tabName = req.params.tab;
@@ -20,10 +42,17 @@ app.get("/login", function (req, res) {
     res.render("login", { userFound: true });
 });
 
-app.get("/signup", function (req, res) {
-    res.render("signup", { userFound: true });
+app.get("/outh2callback", function (req, res) {
+    res.send("OAuth 2 call back is successful");
 });
 
+app.get("/signup", function (req, res) {
+    res.render("signup", { userEmail : "",userFound: false, passwordMatch : true});
+});
+
+app.get("/dashboard", function (req, res) {
+    res.render("signup", { userEmail : "",userFound: false, passwordMatch : true});
+});
 
 app.post("/signin", function (req, res) {
     var email = req.body.email;
@@ -40,38 +69,53 @@ app.post("/signin", function (req, res) {
             res.render("login", { userFound: found });
         }
         else {
-            res.render("dashboard", { user: userAccount.name });
+            res.redirect("dashboard", { user: userAccount.name });
         }
      });
 });
 
-app.post("/createuser", function (req, res) {
+app.post("/createUser", function (req, res) {
     var email = req.body.email;
     var password = req.body.password;
+    var name = req.body.name;
+    var confirmPassword = req.body.confirmPassword;
     var found = false;
-    userModule.UserModel.find({email:email,password:password},function (error, userAccounts) {
+    var passwordDontMatch = false;
+
+    if(confirmPassword !== password)
+    {
+        res.render("signup", { userFound : false, userEmail:"",passwordMatch : false });
+        return;
+    }
+
+    userModule.UserModel.find({email:email},function (error, userAccounts) {
         var userAccount = null;
         if(userAccounts && userAccounts.length > 0)
         {
             found = true;
             userAccount = userAccounts[0];
         }
-        if (!found) {
-            res.render("login", { userFound: found });
+        if (!found) 
+        {
+             var user = new userModule.UserModel({name : name, email : email,password : password,device:"affkahjdfo8jaoidupejfkjoija"});
+             user.save(function (err,user){
+                 if(err) return console.error(err);
+                 console.log(user);
+                 res.render("login",{userFound : true});
+             });
         }
         else {
-            res.render("dashboard", { user: userAccount.name });
+            res.render("signup", { userFound : found, userEmail: userAccount.email,passwordMatch : true });
         }
      });
 });
 
-
-app.get("*", function (req, res) {
-    res.send("Sorry, page not found..");
+http.createServer(app).listen(10000,function () {
+         console.log("Created HTTP server to listen on Port 10000");
 });
 
+https.createServer(options,app).listen(httpsPort,function () {
+    console.log("Created HTTPS server to listen on Port 443");
+});
 
-app.listen(10000, function () {
-    console.log("Server Has Started");
-})
 
